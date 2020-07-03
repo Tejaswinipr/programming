2acb13d3b64575e694c6892080f0ffc53a4e5667
Tejaswinipr <67628985+Tejaswinipr@users.noreply.github.com>
2020-07-03 18:19:56 +0530

    Create logicaloperators.py

---
+++ logicaloperators.py
@@ -0,0 +1,19 @@
+
+a=0
+b=1 
+c=1                   
+
+#logical and operator
+if(a and b and c):
+    print("All are True")
+#logical or operator
+elif(a or b or c):
+    print("Any one may true")
+else:
+    print("All are false")
+    
+#logical not operator
+if(not a):
+    print("true")
+else:
+    print("false")
