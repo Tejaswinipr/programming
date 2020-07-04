c62572ea644c21dabe34705c2b0b8cf11ec1763b
Tejaswinipr <67628985+Tejaswinipr@users.noreply.github.com>
2020-07-04 20:55:07 +0530

    Create paraprint.py

---
+++ paraprint.py
@@ -0,0 +1,5 @@
+#assigning a paragraph to variable and print
+para="""Hi everyone. Welcome to Python programming.
+Python is a High Level Language... because it can run in any platform,
+easy to write and debubg, etc."""
+print(para)
